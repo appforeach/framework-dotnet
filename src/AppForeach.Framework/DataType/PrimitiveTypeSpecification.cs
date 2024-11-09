75bd0fa058b20cdf1001c51263c5dce03ffcc1e3
Artur Karbone <artur.karbone@gmail.com>
2024-11-09 10:35:42 +0200

    introduce PrimitiveTypeSpecification

---
+++ src/AppForeach.Framework/DataType/PrimitiveTypeSpecification.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AppForeach.Framework.DataType
+{
+    internal class PrimitiveTypeSpecification<TType> : IPrimitiveTypeSpecification<TType>
+    {
+        private bool _optional = false;
+        private bool _required = false;
+
+        public IPrimitiveTypeSpecification<TType> Is<TDataType>() where TDataType : IDataType
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPrimitiveTypeSpecification<TType> IsOptional()
+        {
+            _optional = true;
+            return this;
+        }
+
+        public IPrimitiveTypeSpecification<TType> IsRequired()
+        {
+            _required = true;
+            return this;
+        }
+    }
+}
