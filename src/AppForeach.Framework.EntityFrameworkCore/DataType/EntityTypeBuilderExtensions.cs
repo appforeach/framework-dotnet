ab6c457f9e46dcdd3d137637e63d04d861bdadc6
Artur Karbone <artur.karbone@gmail.com>
2024-11-17 14:52:40 +0200

    first draft of entity spec mapping

--- src/AppForeach.Framework.EntityFrameworkCore/DataType/EntityTypeBuilderExtensions.cs
+++ src/AppForeach.Framework.EntityFrameworkCore/DataType/EntityTypeBuilderExtensions.cs
@@ -1,9 +1,32 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using AppForeach.Framework.DataType;
+using AppForeach.Framework.DataType.Facets;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AppForeach.Framework.EntityFrameworkCore.DataType
 {
     public static class EntityTypeBuilderExtensions
     {
-        public static void FromEntitySpecification<T>(this EntityTypeBuilder<T> ext) where T : class { }
+        public static void FromEntitySpecification<T>(this EntityTypeBuilder<T> builder, BaseEntitySpecification<T> entitySpecification) where T : class
+        {
+            //todo: make field name part of specification
+            foreach (var fieldSpecification in entitySpecification.FieldSpecifications)
+            {
+                var facets = fieldSpecification.Value.Configuration;
+
+                var propertyBuilder = builder.Property(fieldSpecification.Key);
+          
+                var requiredFacet = facets.TryGet<FieldRequiredFacet>();
+                if (requiredFacet is not null)
+                {
+                    propertyBuilder.IsRequired(requiredFacet.Required);
+                }
+
+                var maxLengthFacet = facets.TryGet<FieldMaxLengthFacet>();
+                if (requiredFacet is not null)
+                {
+                    propertyBuilder.HasMaxLength(maxLengthFacet.MaxLength);
+                }
+            }
+        }
     }
 }
--- src/EscapeHit.Invoice.Database/Configuration/InvoiceEntityConfiguration.cs
+++ src/EscapeHit.Invoice.Database/Configuration/InvoiceEntityConfiguration.cs
@@ -2,16 +2,25 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using AppForeach.Framework.EntityFrameworkCore.DataType;
+using AppForeach.Framework.DataType;
 
 namespace EscapeHit.Invoice.Database.Configuration
 {
     public class InvoiceEntityConfiguration : IEntityTypeConfiguration<InvoiceEntity>
     {
+        private readonly BaseEntitySpecification<InvoiceEntity> _entitySpecification;
+
+        public InvoiceEntityConfiguration(BaseEntitySpecification<InvoiceEntity> entitySpecification)
+        {
+            _entitySpecification = entitySpecification;
+        }
+
         public void Configure(EntityTypeBuilder<InvoiceEntity> builder)
         {
             //materialze FacetBag
 
-            builder.FromEntitySpecification();
+            //idea: take _entitySpecification from DI?
+            builder.FromEntitySpecification(_entitySpecification);
 
             builder.Property(e => e.CustomerNumber).IsUnicode(false);
         }
