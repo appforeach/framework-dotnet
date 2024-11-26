735a2b70cbc58504adaf3b085290b0db9eba7578
Artur Karbone <artur.karbone@gmail.com>
2024-11-26 08:33:35 +0200

    Introduce PoC migration

--- src/EscapeHit.Invoice.Database/Migrations/20241126062714_ApplyEntitySpecificationPoC.Designer.cs
+++ src/EscapeHit.Invoice.Database/Migrations/20241126062714_ApplyEntitySpecificationPoC.Designer.cs
@@ -3,6 +3,7 @@ using EscapeHit.Invoice.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EscapeHit.Invoice.Database.Migrations
 {
     [DbContext(typeof(InvoiceDbContext))]
-    partial class InvoiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241126062714_ApplyEntitySpecificationPoC")]
+    partial class ApplyEntitySpecificationPoC
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
