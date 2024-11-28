d685ff176e63eae27962b01e1101361784ad9881
Olegs Selskovs <olegselskov@inbox.lv>
2024-11-28 14:44:07 +0200

    implemented exception handler middleware (#19)
    * implemented exception handler middleware
    
    * registered exception handler middleware
    
    * update version
    
    ---------
    
    Co-authored-by: Seļskovs, Oļegs <olegs.selskovs@if.lv>

---
+++ src/AppForeach.Framework/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AppForeach.Framework
+{
+    public class ExceptionHandlerMiddleware : IOperationMiddleware
+    {
+        private readonly IOperationContext context;
+        private readonly IExceptionEventHandler exceptionEventHandler;
+        private readonly IUnhandledExceptionEventHandler unhandledExceptionEventHandler;
+
+        public ExceptionHandlerMiddleware(IOperationContext context, IExceptionEventHandler exceptionEventHandler, IUnhandledExceptionEventHandler unhandledExceptionEventHandler)
+        {
+            this.context = context;
+            this.exceptionEventHandler = exceptionEventHandler;
+            this.unhandledExceptionEventHandler = unhandledExceptionEventHandler;
+        }
+
+        public async Task ExecuteAsync(NextOperationDelegate next)
+        {
+            try
+            {
+                await next();
+            }
+            catch (Exception ex)
+            {
+                var outputState = context.State.Get<OperationOutputState>();
+
+                var exceptionHandlingResult = exceptionEventHandler.OnException(ex);
+
+                if (exceptionHandlingResult.IsHandled)
+                {
+                    outputState.Result = exceptionHandlingResult.Result;
+                }
+
+                var unhandledExceptionHandlingResult = unhandledExceptionEventHandler.OnUnhandledException(ex);
+
+                if (unhandledExceptionHandlingResult.IsHandled)
+                {
+                    outputState.Result = unhandledExceptionHandlingResult.Result;
+                }
+
+                throw;
+            }
+        }
+    }
+}
