f27d8d8853f1e150b01cd09fee6d03ff29181038
Artur Karbone <artur.karbone@gmail.com>
2024-12-18 08:03:06 +0200

    Introduce AppForeach.Framework.Automapper.Tests

---
+++ tests/AppForeach.Framework.Automapper.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,41 @@
+using AppForeach.Framework.Automapper;
+using AppForeach.Framework.Automapper.Metadata;
+using AppForeach.Framework.Mapping;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+public class ServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void AddAutoMapper_ShouldRegisterAutoMapper()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        // Act
+        services.AddAutoMapper<ServiceCollectionExtensionsTests>();
+
+        // Assert
+        var serviceProvider = services.BuildServiceProvider();
+        var mapper = serviceProvider.GetService<AutoMapper.IMapper>();
+        mapper.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public void AddAutoMapper_ShouldRegisterMappingMetadataProvider()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        // Act
+        services.AddAutoMapper<ServiceCollectionExtensionsTests>();
+
+        // Assert
+        var serviceProvider = services.BuildServiceProvider();
+        var metadataProvider = serviceProvider.GetService<IMappingMetadataProvider>();
+
+        metadataProvider
+            .ShouldNotBeNull()
+            .ShouldBeOfType<MappingMetadataProvider>();
+    }
+}
