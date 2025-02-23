ddaa4b0436a95a2167932feb71f8c47af41301b6
Artur Karbone <artur.karbone@gmail.com>
2025-02-23 14:46:04 +0200

    Migrate AutoMapper to 14.0.0

--- src/AppForeach.Framework.AutoMapper/Metadata/MappingMetadataProvider.cs
+++ src/AppForeach.Framework.AutoMapper/Metadata/MappingMetadataProvider.cs
@@ -1,23 +1,24 @@
 ﻿using AppForeach.Framework.Mapping;
 using AutoMapper;
+using AutoMapper.Internal;
 
 namespace AppForeach.Framework.AutoMapper.Metadata;
 internal class MappingMetadataProvider : IMappingMetadataProvider
 {
-    readonly IConfigurationProvider configurationProvider;
-    public MappingMetadataProvider(IConfigurationProvider configurationProvider)
+    readonly IGlobalConfiguration globalConfiguration;
+    public MappingMetadataProvider(IMapper mapper)
     {
-        this.configurationProvider = configurationProvider;
+        this.globalConfiguration = (IGlobalConfiguration)mapper.ConfigurationProvider;
     }
     public IEnumerable<IMappingMetadata> GetMappingMetadata(Type sourceType)
     {
-        IEnumerable<TypeMap> typeMaps = configurationProvider.GetAllTypeMaps().Where(x => x.SourceType == sourceType /*&& x.DestinationType == destinationType*/);
+        IEnumerable<TypeMap> typeMaps = globalConfiguration.GetAllTypeMaps().Where(x => x.SourceType == sourceType /*&& x.DestinationType == destinationType*/);
 
         return typeMaps.Select(t => new MappingMetadata
         {
             SourceType = t.SourceType,
             DestinationType = t.DestinationType,
-            PropertyMaps = t.PropertyMaps.Where(x => x.HasSource).Select(x => new PropertyMap { SourceName = x.SourceMember.Name, DestinationName = x.DestinationName })
+            PropertyMaps = t.PropertyMaps.Where(x => x.SourceMember is not null).Select(x => new PropertyMap { SourceName = x.SourceMember.Name, DestinationName = x.DestinationName })
         }).ToList();
     }
 }
