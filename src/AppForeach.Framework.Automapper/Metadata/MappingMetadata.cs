3c3c1bba4afd276f1132ecbbac52d796cb572606
Viktor Kulkov <viktor.kulkov@appforeach.com>
2025-02-24 17:55:22 +0200

    folder rename

--- src/AppForeach.Framework.Automapper/Metadata/MappingMetadata.cs
+++
@@ -1,9 +0,0 @@
-﻿using AppForeach.Framework.Mapping;
-
-namespace AppForeach.Framework.AutoMapper.Metadata;
-internal class MappingMetadata : IMappingMetadata
-{
-    public Type DestinationType { get; init; }
-    public Type SourceType { get; init; }
-    public IEnumerable<IPropertyMap> PropertyMaps { get; init; }
-}
--- src/AppForeach.Framework.Automapper/Metadata/PropertyMap.cs
+++
@@ -1,8 +0,0 @@
-﻿using AppForeach.Framework.Mapping;
-
-namespace AppForeach.Framework.AutoMapper.Metadata;
-internal class PropertyMap : IPropertyMap
-{
-    public string SourceName { get; init; }
-    public string DestinationName { get; init; }
-}
