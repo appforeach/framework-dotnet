42e3abd59fa684b2422dd84fefdc410789bf041c
Viktor Kulkov <viktor.kulkov@appforeach.com>
2025-03-24 21:08:20 +0200

    postgre sql support

---
+++ src/AppForeach.Framework.EntityFrameworkCore.PostgreSql/PostgreSqlDbOptionsConfigurator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace AppForeach.Framework.EntityFrameworkCore.PostgreSql;
+
+public class PostgreSqlDbOptionsConfigurator : IDbOptionsConfigurator
+{
+    private readonly IEnumerable<ITransactionRetryExceptionHandler> retryExceptionHandlers;
+
+    public PostgreSqlDbOptionsConfigurator(IEnumerable<ITransactionRetryExceptionHandler> retryExceptionHandlers)
+    {
+        this.retryExceptionHandlers = retryExceptionHandlers;
+    }
+
+    public void SetConnection<TDbContext>(DbContextOptionsBuilder<TDbContext> optionsBuilder, DbConnection connection) where TDbContext : DbContext
+    {
+        optionsBuilder.UseNpgsql(connection);
+    }
+
+    public void SetConnectionString<TDbContext>(DbContextOptionsBuilder<TDbContext> optionsBuilder, string connectionString, TransactionRetrySettings? retrySettings = null)
+        where TDbContext : DbContext
+    {
+        optionsBuilder.UseNpgsql(connectionString, sqlOpt =>
+        {
+            if (retrySettings?.Retry ?? false)
+            {
+                sqlOpt.ExecutionStrategy(dp => new CustomPostgreSqlRetryingExecutionStrategy(dp, retrySettings.RetryCount, retrySettings.RetryDelay, retryExceptionHandlers));
+            }
+        });
+    }
+}
---
+++ src/AppForeach.Framework.Hosting.Features.PostgreSql/PostgreSqlFeatureInstaller.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AppForeach.Framework.Microsoft.Extensions.DependencyInjection;
+using AppForeach.Framework.EntityFrameworkCore;
+using AppForeach.Framework.Hosting.Features.Sql;
+using AppForeach.Framework.EntityFrameworkCore.PostgreSql;
+
+namespace AppForeach.Framework.Hosting.Features.PostgreSql;
+
+public class PostgreSqlFeatureInstaller<TDbContext> : SqlFeatureInstaller<TDbContext>
+    where TDbContext : DbContext
+{
+    private readonly PostgreSqlFeatureOption<TDbContext> option;
+
+    public PostgreSqlFeatureInstaller(PostgreSqlFeatureOption<TDbContext> option) : base(option)
+    {
+        this.option = option;
+    }
+
+    public override void SetUpServices(IApplicationFeatureInstallContext installContext, IServiceCollection services)
+    {
+        base.SetUpServices(installContext, services);
+
+
+        services.AddFrameworkModule<PostgreSqlEntityFrameworkComponents>();
+
+        services.AddApplicationStartup<PostgreSqlMigrationStartup<TDbContext>>();
+        services.Configure<PostgreSqlMigrationOptions<TDbContext>>(opt => opt.ConnectionString = connectionString);
+
+        services.AddApplicationStartup<PostgreSqlMigrationStartup<FrameworkDbContext>>();
+        services.Configure<PostgreSqlMigrationOptions<FrameworkDbContext>>(opt =>
+        {
+            opt.ConnectionString = connectionString;
+            opt.DbContextOptions = opt => opt
+                .MigrationsHistoryTable("__FrameworkEFMigrationsHistory", "framework")
+                .MigrationsAssembly(typeof(PostgreSqlEntityFrameworkComponents).Assembly.FullName);
+        });
+
+    }
+}
