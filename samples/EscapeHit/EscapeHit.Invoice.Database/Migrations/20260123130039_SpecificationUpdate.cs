2d44bd6193fef85199d913df95f5f45e937c62d8
Viktor Kulkov <viktor.kulkov@appforeach.com>
2026-01-25 21:36:49 +0200

    net 10 update

---
+++ samples/EscapeHit/EscapeHit.Invoice.Database/Migrations/20260123130039_SpecificationUpdate.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EscapeHit.Invoice.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class SpecificationUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerNumber",
+                table: "Invoices",
+                type: "varchar(5)",
+                unicode: false,
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldUnicode: false,
+                oldMaxLength: 255);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerNumber",
+                table: "Invoices",
+                type: "varchar(255)",
+                unicode: false,
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(5)",
+                oldUnicode: false,
+                oldMaxLength: 5);
+        }
+    }
+}
