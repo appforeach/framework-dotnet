134c960b3399e30fdc2418321767fe7b63dd782c
Viktor Kulkov <viktor.kulkov@appforeach.com>
2026-07-06 19:41:24 +0300

    db context factory support with mediator

---
+++ src/AppForeach.Framework.EntityFrameworkCore/FrameworkDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppForeach.Framework.EntityFrameworkCore;
+
+public class FrameworkDbContextFactory<TContext> 
+    (
+    IDbContextActivator activator
+    ) : IDbContextFactory<TContext>
+    where TContext : DbContext
+{
+    public TContext CreateDbContext()
+        => activator.Activate<TContext>(DbContextOperationEnlistmentStrategy.Optional);
+}
